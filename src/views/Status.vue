69853fdd2296d6479913ef407bd7534c46713f2c
Max Rune <max.rune@gmail.com>
2020-05-27 14:25:25 +0200

    First commit

--- src/views/Status.vue
+++ src/views/Status.vue
@@ -1,14 +1,48 @@
 <template>
-<main></main>
-  
+  <main>
+    <div class="orderinfo">
+      <p>
+        Ordernummer
+        <span>{{this.ordernumber}}</span>
+      </p>
+    </div>
+    <img src="@/assets/graphics/drone.svg" alt="En drönare med en kopp riktigt gott kaffe." />
+    <h1>Din beställning är på väg!</h1>
+    <p>
+      {{this.eta}}
+      <span>minuter</span>
+    </p>
+  </main>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      ordernumber: "#12345",
+      eta: 13
+    };
+  }
+};
 </script>
 
 <style lang="scss" scoped>
+@import url("https://fonts.googleapis.com/css2?family=Work+Sans&display=swap");
 
+.orderinfo {
+  p {
+    font-family: "Work Sans", sans-serif;
+    color: rgba(255, 255, 255, 0.7);
+    span {
+      font-weight: 600;
+    }
+  }
+}
+
+main {
+  background-color: #e5674e;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
 </style>
