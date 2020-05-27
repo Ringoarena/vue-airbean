2ab5e1615cc237e8094c882ce7bc71caed35e21e
Rikard Virta <rikard.virta90@gmail.com>
2020-05-27 10:48:49 +0200

    setup of boilerplate code, view and components

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,30 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <TheHeader />
+    <TheNavigation />
     <router-view/>
+    <TheFooter />
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import TheHeader from "@/components/TheHeader.vue"
+import TheNavigation from "@/components/TheNavigation.vue"
+import TheFooter from "@/components/TheFooter.vue"
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+export default {
+  components:{
+    TheHeader,
+    TheFooter,
+    TheNavigation
   }
+}
+</script>
+
+<style lang="scss">
+*{
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
 }
 </style>
