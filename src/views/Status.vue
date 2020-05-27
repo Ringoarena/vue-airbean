b2de86969d4096460353b1f0961d5ac0c0b842f4
Max Rune <max.rune@gmail.com>
2020-05-27 14:45:23 +0200

    Add basic template layout

--- src/views/Status.vue
+++ src/views/Status.vue
@@ -7,11 +7,12 @@
       </p>
     </div>
     <img src="@/assets/graphics/drone.svg" alt="En drönare med en kopp riktigt gott kaffe." />
-    <h1>Din beställning är på väg!</h1>
-    <p>
+    <h2>Din beställning är på väg!</h2>
+    <p class="eta">
       {{this.eta}}
       <span>minuter</span>
     </p>
+    <button>Ok, cool!</button>
   </main>
 </template>
 
@@ -28,21 +29,47 @@ export default {
 
 <style lang="scss" scoped>
 @import url("https://fonts.googleapis.com/css2?family=Work+Sans&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=PT+Serif:wght@700&display=swap");
+main {
+  background-color: #e5674e;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 2rem;
+}
+
+h2 {
+  font-weight: bold;
+  font-family: "PT Serif", serif;
+  color: #fff;
+}
 
 .orderinfo {
   p {
     font-family: "Work Sans", sans-serif;
     color: rgba(255, 255, 255, 0.7);
+    font-weight: 200;
     span {
       font-weight: 600;
     }
   }
 }
 
-main {
-  background-color: #e5674e;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+.eta {
+  font-family: "Work Sans", sans-serif;
+  color: #fff;
+  font-weight: 700;
+  span {
+    font-weight: 400;
+  }
+}
+
+button{
+    font-family: "PT Serif", serif;
+    font-weight: bold;
+    font-size: 1.5rem;
+    background-color: white;
+    padding: 0.7rem;
+    border: none;
 }
 </style>
