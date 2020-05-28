9e66bdc1dcc840a972b8e1613f90701b0cbae718
Max Rune <max.rune@gmail.com>
2020-05-28 15:11:07 +0200

    Add order history component, styling and structure

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,39 +1,45 @@
 <template>
-  <main>
-    <div class="profile">
-      <figure>
-        <img src="@/assets/graphics/eva-cortado.jpg" alt="Profilbild" />
-        <h3>Förnamn Efternamn</h3>
-        <p>epost@example.com</p>
-      </figure>
+  <main class="profile">
+    <figure>
+      <img src="@/assets/graphics/eva-cortado.jpg" alt="Profilbild" />
+      <h3>Förnamn Efternamn</h3>
+      <p>epost@example.com</p>
+    </figure>
+    <div class="orderHistory">
+      <h3>Orderhistorik</h3>
+      <OrderHistory />
+      <OrderHistory />
     </div>
   </main>
 </template>
 
 <script>
+import OrderHistory from "@/components/OrderHistory.vue";
 export default {
-    props: ["user"],
+  components: {
+    OrderHistory
+  },
+  props: {},
 
-    computed:{
-        computedUser(){
-            return 
-        }
-    },
+  computed: {
+    computedUser() {
+      return this.$store.state.user;
+    }
+  },
 
-    methods: {
-        getUser(){
-        },
-    },
+  methods: {
+    getUser() {}
+  }
 };
-
 </script>
 
 <style lang="scss" scoped>
 @import url("https://fonts.googleapis.com/css2?family=PT+Serif:wght@700&display=swap");
 @import url("https://fonts.googleapis.com/css2?family=Work+Sans&display=swap");
 
-main,
+main.profile,
 figure {
+  padding: 1.2rem;
   color: #fff;
   background-color: #2f2926;
   display: flex;
@@ -42,6 +48,22 @@ figure {
 
   h3 {
     font-family: "PT Serif", serif;
+    margin-bottom: 1.2rem;
+  }
+
+  p{
+    font-family: "Work Sans", sans-serif;
+  }
+}
+
+.orderHistory {
+  background-color: #2f2926;
+  padding: 1.2rem;
+}
+
+figure{
+  img{
+    border-radius: 50%;
   }
 }
 </style>
